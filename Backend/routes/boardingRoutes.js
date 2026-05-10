f26693438bccd6610f22e2931ac47d1c95b509e4
oshadi <bawanioshadi0@gmail.com>
2026-05-10 09:58:08 +0530

    create the ownerboarding routes

--- Backend/routes/boardingRoutes.js
+++ Backend/routes/boardingRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createBoarding } from "../controllers/boardingController.js";
+import { createBoarding,getOwnerBoardings } from "../controllers/boardingController.js";
 
 import { protect } from "../middleware/authMiddleware.js";
 import { isOwner } from "../middleware/roleMiddleware.js";
@@ -9,6 +9,7 @@ const router = express.Router();
 
 
 router.post("/createBoarding", protect, isOwner, createBoarding);
+router.get("/ownerBoardings", protect, isOwner, getOwnerBoardings);
 
 
 
