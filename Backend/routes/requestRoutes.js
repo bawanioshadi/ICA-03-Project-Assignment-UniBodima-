06208015677370ac17ccf6f523137259be55af50
oshadi <bawanioshadi0@gmail.com>
2026-05-10 22:34:40 +0530

    getStudentRequests endpoint created

--- Backend/routes/requestRoutes.js
+++ Backend/routes/requestRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {createRequest,getOwnerRequests,acceptRequest} from "../controllers/requestController.js";
+import {createRequest,getOwnerRequests,acceptRequest,getStudentRequests} from "../controllers/requestController.js";
 
 import { protect } from "../middleware/authMiddleware.js";
 import { isOwner } from "../middleware/roleMiddleware.js";
@@ -8,6 +8,7 @@ const router = express.Router();
 
 router.post("/create", protect, createRequest);
 router.get("/ownerRequests", protect, isOwner, getOwnerRequests);
+router.get("/studentRequests", protect, getStudentRequests);
 
 router.put("/accept/:id", protect, isOwner, acceptRequest);
 
