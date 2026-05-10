213002e5def23562f39c50828ab0394f9fc27540
oshadi <bawanioshadi0@gmail.com>
2026-05-10 12:24:08 +0530

    boarding delete endpoint created

--- Backend/routes/boardingRoutes.js
+++ Backend/routes/boardingRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createBoarding,getOwnerBoardings, updateBoarding} from "../controllers/boardingController.js";
+import { createBoarding,getOwnerBoardings, updateBoarding,deleteBoarding} from "../controllers/boardingController.js";
 
 import { protect } from "../middleware/authMiddleware.js";
 import { isOwner } from "../middleware/roleMiddleware.js";
@@ -11,6 +11,7 @@ const router = express.Router();
 router.post("/createBoarding", protect, isOwner, createBoarding);
 router.get("/ownerBoardings", protect, isOwner, getOwnerBoardings);
 router.put("/updateBoarding/:id", protect, isOwner, updateBoarding);
+router.delete("/deleteBoarding/:id", protect, isOwner, deleteBoarding);
 
 
 
