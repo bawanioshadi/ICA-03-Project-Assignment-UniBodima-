a19154dec8f4ab34dfd965454db5a37983ab0020
oshadi <bawanioshadi0@gmail.com>
2026-05-10 22:24:27 +0530

    accept endpoint created

--- Backend/routes/requestRoutes.js
+++ Backend/routes/requestRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {createRequest,getOwnerRequests} from "../controllers/requestController.js";
+import {createRequest,getOwnerRequests,acceptRequest} from "../controllers/requestController.js";
 
 import { protect } from "../middleware/authMiddleware.js";
 import { isOwner } from "../middleware/roleMiddleware.js";
@@ -9,6 +9,7 @@ const router = express.Router();
 router.post("/create", protect, createRequest);
 router.get("/ownerRequests", protect, isOwner, getOwnerRequests);
 
+router.put("/accept/:id", protect, isOwner, acceptRequest);
 
 
 export default router;
