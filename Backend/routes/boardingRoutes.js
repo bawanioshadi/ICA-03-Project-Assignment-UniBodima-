8b03553ed1a8472ee2ded59a0ee6facbc3b90c6f
oshadi <bawanioshadi0@gmail.com>
2026-05-10 12:02:28 +0530

    updated the boarding endpoint

--- Backend/routes/boardingRoutes.js
+++ Backend/routes/boardingRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createBoarding,getOwnerBoardings } from "../controllers/boardingController.js";
+import { createBoarding,getOwnerBoardings, updateBoarding} from "../controllers/boardingController.js";
 
 import { protect } from "../middleware/authMiddleware.js";
 import { isOwner } from "../middleware/roleMiddleware.js";
@@ -10,6 +10,7 @@ const router = express.Router();
 
 router.post("/createBoarding", protect, isOwner, createBoarding);
 router.get("/ownerBoardings", protect, isOwner, getOwnerBoardings);
+router.put("/updateBoarding/:id", protect, isOwner, updateBoarding);
 
 
 
