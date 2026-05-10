f43c430ec18b293dfcbdb73a50d9d998b3911250
oshadi <bawanioshadi0@gmail.com>
2026-05-10 22:09:26 +0530

    ownerRequests endpoint

--- Backend/routes/requestRoutes.js
+++ Backend/routes/requestRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {createRequest} from "../controllers/requestController.js";
+import {createRequest,getOwnerRequests} from "../controllers/requestController.js";
 
 import { protect } from "../middleware/authMiddleware.js";
 import { isOwner } from "../middleware/roleMiddleware.js";
@@ -7,6 +7,7 @@ import { isOwner } from "../middleware/roleMiddleware.js";
 const router = express.Router();
 
 router.post("/create", protect, createRequest);
+router.get("/ownerRequests", protect, isOwner, getOwnerRequests);
 
 
 
