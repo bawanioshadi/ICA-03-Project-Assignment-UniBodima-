81aa085ebf2381058d19656eaaf13784b53216b3
oshadi <bawanioshadi0@gmail.com>
2026-05-10 16:05:23 +0530

    create reqvest controller

---
+++ Backend/controllers/requestController.js
@@ -0,0 +1,35 @@
+import Request from "../models/Request.js";
+// CREATE REQUEST (STUDENT)
+export const createRequest = async (req, res) => {
+  try {
+    const { boardingId, studentName, studentPhone } = req.body;
+    if (!boardingId || !studentName || !studentPhone) {
+      return res.status(400).json({ status: "fail", message: "All fields are required" });
+    }
+
+    const request = await Request.create({
+      studentId: req.user.id,
+      boardingId,
+      studentName,
+      studentPhone
+    });
+
+    res.status(201).json({ status: "success", message: "Request send successfully", request });
+
+  } catch (error) {
+    res.status(500).json({ status: "fail", message: error.message });
+  }
+};
+// GET OWNER'S REQUESTS
+export const getOwnerRequests = async (req, res) => {
+  try {
+    const requests = await Request.find()
+      .populate("boardingId")
+      .populate("studentId", "name phone");
+
+    res.json({ status: "success", requests });
+
+  } catch (error) {
+    res.status(500).json({ status: "fail", message: error.message });
+  }
+};
